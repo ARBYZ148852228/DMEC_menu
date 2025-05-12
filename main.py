1f3de4f77bd32e1f2cb2af3c496788ea69da31b9
ARBYZ148852228 <anonim15255@gmail.com>
2025-05-12 21:47:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,81 +1,13 @@
 from tkinter import *
 from pickle import load, dump
 
-# область функций
-def set_status():
-    pass
-
-def pause_toggle():
-    pass
-
-
-def menu_toggle():
-    pass
-
-
-def key_handler(event):
-    pass
-
-def check_finish():
-    pass
-
-
-def menu_enter():
-    pass
-
-
-def game_new():
-    pass
-
-
-def game_resume():
-    pass
-
-
-def game_save():
-    pass
-
-
-def game_load():
-    pass
-
-
-def game_exit():
-    pass
-
-
-def menu_show():
-    pass
-
-
-def menu_hide():
-    pass
-
-
-def menu_up():
-    pass
-
-
-def menu_down():
-    pass
-
-
-def menu_update():
-    pass
-
-
-def menu_create(canvas):
-    pass
-
-
-# область переменных
+# Область переменных
 game_width = 800
 game_height = 800
 menu_mode = True
 menu_options = ['Возврат в игру', 'Новая игра', 'Сохранить', 'Загрузить', 'Выход']
 menu_current_index = 3
 menu_options_id = []
-
 
 KEY_UP = 87
 KEY_DOWN = 83
@@ -99,8 +31,57 @@ SPEED = 12
 game_over = False
 pause = False
 
-game_width = 800
-game_height = 800
+# Область функций
+def set_status():
+    pass
+
+def pause_toggle():
+    pass
+
+def menu_toggle():
+    pass
+
+def key_handler(event):
+    pass
+
+def check_finish():
+    pass
+
+def menu_enter():
+    pass
+
+def game_new():
+    pass
+
+def game_resume():
+    pass
+
+def game_save():
+    pass
+
+def game_load():
+    pass
+
+def game_exit():
+    pass
+
+def menu_show():
+    pass
+
+def menu_hide():
+    pass
+
+def menu_up():
+    pass
+
+def menu_down():
+    pass
+
+def menu_update():
+    pass
+
+def menu_create(canvas):
+    pass
 
 # Окно и объекты
 window = Tk()
@@ -108,30 +89,16 @@ window.title('DMEC')
 
 canvas = Canvas(window, width=game_width, height=game_height, bg='white')
 canvas.pack()
+
+# Создание объектов игры
 menu_create(canvas)
-player1 = canvas.create_rectangle(x1,
-                                  y1,
-                                  x1 + player_size,
-                                  y1 + player_size,
-                                  fill=player1_color)
-player2 = canvas.create_rectangle(x2,
-                                  y2,
-                                  x2 + player_size,
-                                  y2 + player_size,
-                                  fill=player2_color)
-finish_id = canvas.create_rectangle(x_finish,
-                                    0,
-                                    x_finish + 10,
-                                    game_height,
-                                    fill='black')
+player1 = canvas.create_rectangle(x1, y1, x1+player_size, y1+player_size, fill=player1_color)
+player2 = canvas.create_rectangle(x2, y2, x2+player_size, y2+player_size, fill=player2_color)
+finish_id = canvas.create_rectangle(x_finish, 0, x_finish+10, game_height, fill='black')
+text_id = canvas.create_text(x1, game_height-50, anchor=SW, font=('Arial', '25'), text='Вперед!')
 
-text_id = canvas.create_text(x1,
-                             game_height - 50,
-                             anchor=SW,
-                             font=('Arial', '25'),
-                             text='Вперед!')
-
-
-# Функции обратного вызова
+# Назначение обработчиков клавиш
 window.bind('<KeyRelease>', key_handler)
+
+# Запуск главного цикла окна
 window.mainloop()
